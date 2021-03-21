953f560e9855fd2fbdeb7b1983c1b5db69ed54c1
Hayden Young <hi@hbjy.dev>
2021-03-21 20:55:54 +0000

    feat: hide logs behind ENV!=production

--- middlewares/log.go
+++ middlewares/log.go
@@ -3,14 +3,19 @@ package middlewares
 import (
 	"log"
 	"net/http"
+	"os"
 	"time"
 )
 
 func LogMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		now := time.Now()
-		next.ServeHTTP(w, r)
-		since := time.Since(now).Milliseconds
-		log.Printf("%v %v -- %v", r.Method, r.URL.Path, since())
+		if os.Getenv("ENV") != "production" {
+			now := time.Now()
+			next.ServeHTTP(w, r)
+			since := time.Since(now).Milliseconds
+			log.Printf("%v %v -- %v", r.Method, r.URL.Path, since())
+		} else {
+			next.ServeHTTP(w, r)
+		}
 	})
 }
